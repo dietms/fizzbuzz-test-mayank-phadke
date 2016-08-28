f4332506c58614ab5888b150253aa7899b114dc7
Mayank Phadke <mayank.phadke1996@gmail.com>
2016-08-28 23:16:07 +0530

    #include<iostream.h> int main() {   for(int i=1;i<=100;i++)   {     if(i%3==0 && i%5==0)       cout<<"FizzBuzz\n";     else if(i%5==0)       cout<<"Buzz\n";     else if(i%3==0)       cout<<"Fizz\n";     else       cout<<i<<endl;   }   return 0; }

---
+++ Desktop/FizzBuzz.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+int main()
+{
+  for(int i=1;i<=100;i++)
+  {
+    if(i%3==0 && i%5==0)
+      cout<<"FizzBuzz\n";
+    else if(i%5==0)
+      cout<<"Buzz\n";
+    else if(i%3==0)
+      cout<<"Fizz\n";
+    else
+      cout<<i<<endl;
+  }
+  return 0;
+}
